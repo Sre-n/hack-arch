3b3eb924aefc448c015f437df3b9c641c13dd7fb
Sreni Saji <92539781+Sre-n@users.noreply.github.com>
2022-06-22 23:51:28 +0530

    Update import.py

--- import.py
+++ import.py
@@ -3,6 +3,9 @@ import pytesseract
 import numpy as np
 import imutils
 import cv2
+from pyimagesearch.anpr import PyImageSearchANPR
+from imutils import paths
+import argparse
 class PyImageSearchANPR:
 	def __init__(self, minAR=4, maxAR=5, debug=False):
 		self.minAR = minAR
